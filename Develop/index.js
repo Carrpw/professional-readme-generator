b7fbf3aee5ae07bf6baf392c30a1d0e4974539ba
Paul Carr <pwcarr15@gmail.com>
2020-11-25 16:21:20 -0500

    Add index changes and .gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,7 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // function to initialize program
@@ -14,3 +15,16 @@ function init() {
 
 // function call to initialize program
 init();
+
+// I need to make a function to write a readme
+// Maybe use fs.appendfile in order to make and write the readme
+// I need to create questions about what should be on the readme
+// See acceptance criteria for ideas on question substance
+// Readmd is an html file apparently?
+// If so then I can use different tags to make h1, h2, p, ect.. 
+// I need to mae a node_modules/ and .ds_store(what is that?)
+// To make node-modules I need to use npm init
+// Dependency is inquirer, do I need others?
+// How do I include a video of user flow?
+// Include screenshots
+// 
