99ab11e11c3b71312be78848079986658398762e
Paul Carr <pwcarr15@gmail.com>
2020-11-29 13:18:33 -0500

    added questions to the array and started the write file function.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,54 @@
+const inquirer = require('inquirer');
+const  fs = require('fs');
+
+
 // array of questions for user
 const questions = [
-
+    
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What would you like the title of the Readme.md to be?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?',
+        },
+        {
+            type: 'checkbox',
+            name: 'table of contents',
+            message: 'What would you like in your table of contents?',
+            choices: ['Title', 'Description', 'Table of Contents', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'],
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'What are the installation instructions for your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage of this program?'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who were the contributors to this project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            mesage: 'Tests?',
+        },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    
+    inquirer.prompt(questions).then((data) => {
+        fs.appendFile('readme.md', JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.error(err) : console.log('File Created!')
+    )});
 }
 
 // function to initialize program
@@ -15,6 +58,9 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+// fs.appendFile("readme.md", )
 
 // I need to make a function to write a readme
 // Maybe use fs.appendfile in order to make and write the readme
---
+++ Develop/readme.md
@@ -0,0 +1,20 @@
+undefined
+{
+	"title": "a",
+	"description": "b",
+	"table of contents": [
+		"Title",
+		"Description",
+		"Table of Contents",
+		"Installation",
+		"Usage",
+		"License",
+		"Contributing",
+		"Tests",
+		"Questions"
+	],
+	"instructions": "d",
+	"usage": "e",
+	"contributors": "f",
+	"tests": "g"
+}
